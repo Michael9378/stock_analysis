3578cde7b9ee23eb8d4c78a5d5e60fb9dba73cd4
Michael Scott <mjscott7@asu.edu>
2017-08-28 12:03:20 -0700

    Start earnings scraper file

---
+++ JavaScript/earningsScraper.js
@@ -0,0 +1,3 @@
+// http://fundamentals.nasdaq.com/redpage.asp?selected=FCCY&market=NASDAQGM&page=1
+
+// http://fundamentals.nasdaq.com/redpage.asp?selected=[STOCK_SYMBOL]&market=NASDAQGM&page=[PAGE_NUM]
